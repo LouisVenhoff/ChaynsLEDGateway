fd73e5594399d1064bc5d35401703e07860f187b
Louis Venhoff <Louis.venhoff@gmx.net>
2026-08-25 21:03:04 +0200

    Established connection to chayns test broker

--- mqtt-radio-gateway/src/main.cpp
+++ mqtt-radio-gateway/src/main.cpp
@@ -7,5 +7,9 @@ int main() {
     mqtt::async_client* mqtt_cl;
 
     int result = connectToBroker(mqtt_cl);
+
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get();
+
     return 0;
 }
--- mqtt-radio-gateway/src/mqtt.cpp
+++ mqtt-radio-gateway/src/mqtt.cpp
@@ -1,26 +1,30 @@
 #include "mqtt.h"
 
-const string SERVER_ADDRESS("tcp://localhost:1883");
-const string CLIENT_ID("test_subscriber");
-const string TOPIC("test_topic");
+const string SERVER_ADDRESS("mqtt://mqtt.chayns.io:1883");
+const string CLIENT_ID("99825-09414-o3LhwJr7");
+const string PWD("It2lVpoXgyd8hdNqUS24");
+const string TOPIC("test-louis-dev");
 
-// class callback : public virtual mqtt::callback {
-//     void message_arrived(mqtt::const_message_ptr msg) override {
-//         std::cout << "Message received:" << msg->get_payload_str() << std::endl;
-//     }
-// };
-
+class callback : public virtual mqtt::callback {
+    void message_arrived(mqtt::const_message_ptr msg) override {
+        std::cout << "Message received:" << msg->get_payload_str() << std::endl;
+    }
+};
 
 
 int connectToBroker(mqtt::async_client* mqtt_client){
     mqtt::async_client client(SERVER_ADDRESS, CLIENT_ID);
 
-    //callback cb;
+    callback cb;
+
+    client.set_callback(cb);
 
     mqtt::connect_options conOpts;
 
     conOpts.set_keep_alive_interval(20);
     conOpts.set_clean_session(true);
+    conOpts.set_user_name(CLIENT_ID);
+    conOpts.set_password(PWD);
 
     try{
         client.connect(conOpts) -> wait();
@@ -32,17 +36,22 @@ int connectToBroker(mqtt::async_client* mqtt_client){
     }
     catch(const mqtt::exception& exc){
         cout << "Connection failed!" << exc.what() << endl;
-        return 1;
+        return -1;
     }
 
     return 0;
 }
 
-
-void sendMessage(){
-    cout << "Hello from MQTT Test";
-}
-
-int subscribeToBroker(){
+int subscribeTopic(mqtt::async_client* mqtt_client){
     
+    try{
+        mqtt_client->subscribe(TOPIC, 1)->wait();
+    }
+    catch(mqtt::exception& exc){
+        cout << "Connection failed!" << exc.what() << endl;
+        return -1;
+    }
+
+    std::cout << "Subscribed to" << TOPIC << std::endl;
 }
+
