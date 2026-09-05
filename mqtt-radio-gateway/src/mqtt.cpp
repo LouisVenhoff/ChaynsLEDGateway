51c2c5136ed08daf968872eb6de3eed4b8637661
Louis Venhoff <Louis.venhoff@gmx.net>
2026-09-06 01:27:43 +0200

    Send values via UART

--- mqtt-radio-gateway/src/mqtt.cpp
+++ mqtt-radio-gateway/src/mqtt.cpp
@@ -9,8 +9,19 @@ const string PWD("r2VjIYeNnBXmtPT3qp2x");
 const string TOPIC("chayns-led");
 
 class callback : public virtual mqtt::callback {
+    
+    int fd = -1;
+
+    public:void setFd(int fileDescriptor){
+        fd = fileDescriptor;
+    }
+
     void message_arrived(mqtt::const_message_ptr msg) override {
         //std::cout << "Message received:" << msg->get_payload_str() << std::endl;
+
+        if(fd == -1){
+            std::cout << "UART not connected!" << std::endl;
+        }
 
         json data = json::parse(msg->get_payload_str());
 
@@ -38,12 +49,21 @@ class callback : public virtual mqtt::callback {
         command.brightness = brightness;
 
         vector<uint8_t> byteData =  generateProtocolBytes(command);
+
+        sendSerial(fd, byteData);
     }
 };
 
 
 int connectToBroker(mqtt::async_client& mqtt_client){
+    
+    int fd = -1;
+
+    beginSerial(&fd);
+    
     static callback cb;
+
+    cb.setFd(fd);
 
     mqtt_client.set_callback(cb);
 
