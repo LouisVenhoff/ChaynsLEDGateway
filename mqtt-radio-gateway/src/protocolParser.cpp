7b2c22257fb443eda6f776772f1f0a700a31533d
Louis Venhoff <Louis.venhoff@gmx.net>
2026-09-05 23:58:24 +0200

    Fixed bug while generating bytes for protocol

--- mqtt-radio-gateway/src/protocolParser.cpp
+++ mqtt-radio-gateway/src/protocolParser.cpp
@@ -4,7 +4,7 @@ int send(vector<uint8_t> word){
     for(int i = 0; i <= 6; i++){
         std::cout << std::bitset<8>(word[i]) << " ";
 
-        if(i == 5){
+        if(i == 6){
             std::cout << std::endl;
         }
     }
@@ -13,12 +13,12 @@ int send(vector<uint8_t> word){
 int generateProtocolBytes(cmd command){
     vector<uint8_t> word;
 
-    word[0] = command.address;
-    word[1] = command.enabled;
-    word[2] = command.colorR;
-    word[3] = command.colorG;
-    word[4] = command.colorB;
-    word[5] = command.brightness;
+    word.push_back(command.address);
+    word.push_back(command.enabled);
+    word.push_back(command.colorR);
+    word.push_back(command.colorG);
+    word.push_back(command.colorB);
+    word.push_back(command.brightness);
 
     uint8_t crc = calculateCRC8(&word[0], 6);
 
